28980e88c56e07052b57e3710dbfb377d7612209
chris.zhu <zcf02045093@gmail.com>
2019-01-03 00:11:18 -0800

    commit the change

--- QuantitativeTransaction/DataProcess.py
+++ QuantitativeTransaction/DataProcess.py
@@ -374,7 +374,7 @@ class DataProcess(object):
         
 
 if __name__ == '__main__':
-    dataDate = '2019-01-01'
+    dataDate = '2019-01-02'
 #     wanke = DataProcess('000002',dataDate,'D')
 #     wanke.makeGenData()
 #     wanke.saveAsGeneratedData()
