099e8171fe31b6bd85671529ad3b2457cd099242
chris.zhu <zcf02045093@gmail.com>
2019-01-16 01:12:18 -0800

    change test for dp

--- QuantitativeTransaction/StockDataProcessingTest.py
+++ QuantitativeTransaction/StockDataProcessingTest.py
@@ -2,23 +2,25 @@ import StockData as sd
 import DataProcess as dp
 
 # dataDate = '2019-01-07'
-sdate = sd.SingletonDate.GetInstance('2019-01-09')
-# sdate = sd.SingletonDate.GetInstance()
-zz500StockListFileUrl  = 'http://www.csindex.com.cn/uploads/file/autofile/cons/000905cons.xls'
-hs300StockListFileUrl  = 'http://www.csindex.com.cn/uploads/file/autofile/cons/000300cons.xls'
-indexCode, filename= sd.getNameAndCode(hs300StockListFileUrl)
-hs300CodeList = sd.getChinaStockList(hs300StockListFileUrl, filename)
-print('The length of hs300CodeList is: %d!' % (len(hs300CodeList)))
- 
-indexCode, filename= sd.getNameAndCode(zz500StockListFileUrl)
-zz500CodeList = sd.getChinaStockList(zz500StockListFileUrl, filename)
-print('The length of zz500CodeList is: %d' % (len(zz500CodeList)))
- 
+# sdate = sd.SingletonDate.GetInstance('2019-01-09')
+sdate = sd.SingletonDate.GetInstance()
+# zz500StockListFileUrl  = 'http://www.csindex.com.cn/uploads/file/autofile/cons/000905cons.xls'
+# hs300StockListFileUrl  = 'http://www.csindex.com.cn/uploads/file/autofile/cons/000300cons.xls'
+# indexCode, filename= sd.getNameAndCode(hs300StockListFileUrl)
+# hs300CodeList = sd.getChinaStockList(hs300StockListFileUrl, filename)
+# print('The length of hs300CodeList is: %d!' % (len(hs300CodeList)))
+#   
+# indexCode, filename= sd.getNameAndCode(zz500StockListFileUrl)
+# zz500CodeList = sd.getChinaStockList(zz500StockListFileUrl, filename)
+# print('The length of zz500CodeList is: %d' % (len(zz500CodeList)))
+#  
 myCodeList = ['600030','600036','600061','600893','600498','300033','600547','300383','002716','600109','002353','300059']
+# myCodeList = ['300674']
 print('The length of myCodeList is: %d' % (len(myCodeList)))
-  
-codeList = hs300CodeList + zz500CodeList + myCodeList
-codeList = list(set(codeList))
+#   
+# codeList = hs300CodeList + zz500CodeList + myCodeList
+# codeList = list(set(codeList))
+codeList = list(set(myCodeList))
 print('The length of codeList is: %d' % len(codeList))
 
 
